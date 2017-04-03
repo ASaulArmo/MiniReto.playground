e38300fc850760a60f69a63b681eaf14cd1e32a6
ASaulArmo <saul3753@gmail.com>
2017-04-02 21:09:24 -0500

    Proyecto semana dos
    Mini reto uno para creación e identificación de números 0-100

---
+++ Contents.swift
@@ -0,0 +1,22 @@
+//miniReto de progra Curso de David Alejandro Escárcega del ITESM
+//A. Saúl Armendáriz Morín -- > Autor
+
+import UIKit
+
+for inputs in 0...100{
+
+if inputs % 5 == 0{
+    
+    print("\(inputs) bingo!!!")
+    }
+if inputs % 2 == 0{
+        
+        print("\(inputs) par!!!")
+}else {
+    
+        print("\(inputs) impar!!!")
+    }
+if inputs >= 30 && inputs <= 40 {
+        print ("\(inputs) Viva Swift!!!")
+    }
+}
